60d816f771e4c391632129423e655daba23bfed2
Noa LELCERC <leclercn@bts-sn.lrq>
2025-11-24 11:57:06 +0100

    update

--- interfaceGraphique.py
+++ interfaceGraphique.py
@@ -3,36 +3,38 @@
 from tkinter import * 
 
 class IHM:
-
+    
     def __init__(self):
         self.fenetre = Tk()
-        self.fenetre = IHM()
-        self.label = Label(self.fenetre, text='question')
+        self.inter.demander_choix
+        self.label = Label(fenetre, text='question')
         self.label.pack()    
 
         self.fenetre['bg']='white'
 
         # frame 1
-        Frame1 = Frame(fenetre, borderwidth=2, relief=GROOVE)
+        Frame1 = Frame(self.fenetre, borderwidth=2, relief=GROOVE)
         Frame1.pack(side=LEFT, padx=50, pady=30)
         self.bouton1 = Button(Frame1, text="Beurk", height=4, width=9, background="red")
         self.bouton1.pack()
 
-        Frame1 = Frame(fenetre, borderwidth=2, relief=GROOVE)
+        Frame1 = Frame(self.fenetre, borderwidth=2, relief=GROOVE)
         Frame1.pack(side=LEFT, padx=50, pady=30)
         Bouton = Button(Frame1, text="Bof", height=4, width=9, background="orange")
         Bouton.pack()
 
-        Frame1 = Frame(fenetre, borderwidth=2, relief=GROOVE)
+        Frame1 = Frame(self.fenetre, borderwidth=2, relief=GROOVE)
         Frame1.pack(side=LEFT, padx=50, pady=30)
         Bouton = Button(Frame1, text="Bon", height=4, width=9, background="yellow")
         Bouton.pack()
 
-        Frame1 = Frame(fenetre, borderwidth=2, relief=GROOVE)
+        Frame1 = Frame(self.fenetre, borderwidth=2, relief=GROOVE)
         Frame1.pack(side=LEFT, padx=50, pady=30)
         Bouton = Button(Frame1, text="Délicieux", height=4, width=9, background="green")
         Bouton.pack()
-
+        
+        
+        self.fenetre.mainloop()
     def demander_choix(self, question,listeChoix):
         """modifier les valeurs de question et liste choix
         puis retourner le choix cliqué par l'utilisateur"""
@@ -40,8 +42,7 @@ class IHM:
         self.label.config(text=question)
 
         self.bouton1.config(text=listeChoix[0]) 
+    
 
     
-        self.fenetre.mainloop()
-inter = IHM()
-inter.demander_choix
+        
